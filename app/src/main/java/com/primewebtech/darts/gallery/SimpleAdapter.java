73d7e488e8d28faf5627742832179857f521a520
Ben Ebsworth <ben.ebsworth@gmail.com>
2017-05-13 11:12:59 +1000

    working gallery layout with smoothed animations

--- app/src/main/java/com/primewebtech/darts/gallery/SimpleAdapter.java
+++ app/src/main/java/com/primewebtech/darts/gallery/SimpleAdapter.java
@@ -1,15 +1,22 @@
 package com.primewebtech.darts.gallery;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageSize;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.primewebtech.darts.R;
 
 import java.io.File;
@@ -25,21 +32,23 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SimpleView
     private static final int COUNT = 100;
     private File pictureDirectory;
     private List<File> sortedFiles;
-    private int THUMBSIZE_W = 90;
-    private int THUMBSIZE_H = 130;
+    private int THUMBSIZE_W = 30;
+    private int THUMBSIZE_H = 30;
+    private ImageLoader mImageLoader;
+    private final ImageLoaderConfiguration mConfig;
 
     private final Context mContext;
     private final List<GalleryItem> mItems;
     private int mCurrentItemId = 0;
 
     public static class SimpleViewHolder extends RecyclerView.ViewHolder {
-        public final TextView title;
+//        public final TextView title;
         public final ImageView thumbnail;
 
         public SimpleViewHolder(View view) {
             // initialises the viewholder for view hydration.
             super(view);
-            title = (TextView) view.findViewById(R.id.title);
+//            title = (TextView) view.findViewById(R.id.title);
             thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
 
         }
@@ -47,7 +56,10 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SimpleView
 
     public SimpleAdapter(Context context) {
         mContext = context;
+
         mItems = new ArrayList<>(COUNT);
+        mConfig = new ImageLoaderConfiguration.Builder(mContext).build();
+        ImageLoader.getInstance().init(mConfig);
         /*
         The adapter will initialise with 100 items. The sectionViewAdapter will then be responsible
         for sectioning these items according to the firstPosition????
@@ -71,11 +83,32 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SimpleView
     }
 
     @Override
-    public void onBindViewHolder(SimpleViewHolder holder, final int position) {
+    public void onBindViewHolder(final SimpleViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder:"+Integer.toString(mItems.get(position).mPosition));
-        holder.title.setText(Integer.toString(mItems.get(position).mPosition));
-        holder.thumbnail.setImageBitmap(Util.decodeSampledBitmapFromFile(mItems.get(position).mItemPath.getPath(),
-                THUMBSIZE_W, THUMBSIZE_H));
+//        holder.title.setText(Integer.toString(mItems.get(position).mPosition));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Uri photoURI = FileProvider.getUriForFile(mContext,
+                        mContext.getApplicationContext().getPackageName() + ".fileprovider",
+                        mItems.get(position).mItemPath);
+                intent.setDataAndType(photoURI, "image/*");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                mContext.startActivity(intent);
+            }
+        });
+        ImageSize targetSize = new ImageSize(80, 50);
+        mImageLoader = ImageLoader.getInstance();
+        mImageLoader.loadImage("file:///"+mItems.get(position).mItemPath.getPath(), targetSize, null, new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                // Do whatever you want with Bitmap
+                holder.thumbnail.setImageBitmap(loadedImage);
+            }
+        });
     }
 
     public void addItem(GalleryItem item) {
